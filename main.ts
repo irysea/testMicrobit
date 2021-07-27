47b852ac1b2aedc3832c6cf11bc6cac9c6ccee4b
irysea <78557937+irysea@users.noreply.github.com>
2021-07-28 05:00:37 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     basic.pause(1000)
-    basic.showString("Hello!")
+    basic.showString(convertToText(input.compassHeading()))
     basic.pause(1000)
     basic.clearScreen()
 })
