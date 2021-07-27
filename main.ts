5748af238ca9ad0b899452a40d741a0a67ef8e7b
irysea <78557937+irysea@users.noreply.github.com>
2021-07-28 05:14:42 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 basic.forever(function () {
     basic.pause(1000)
-    basic.showString(convertToText(input.compassHeading()))
+    if (input.compassHeading() > 45 && input.compassHeading() < 135) {
+        basic.showString("" + (images.arrowImage(ArrowNames.North)))
+    }
     basic.pause(1000)
     basic.clearScreen()
 })
