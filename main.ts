4099872395b3238278cf0fe157aa2c7e7b722538
irysea <78557937+irysea@users.noreply.github.com>
2021-07-28 04:50:05 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -2,9 +2,5 @@ basic.showString("Hello!")
 basic.pause(1000)
 basic.clearScreen()
 basic.forever(function () {
-    basic.showString("x" + input.magneticForce(Dimension.X))
-    basic.pause(500)
-    basic.showString("y" + input.magneticForce(Dimension.Y))
-    basic.pause(500)
-    basic.showString("z" + input.magneticForce(Dimension.Z))
+	
 })
