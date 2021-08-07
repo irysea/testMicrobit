13ae7f72897c81602321ae091d5fd06fc7a0d982
irysea <78557937+irysea@users.noreply.github.com>
2021-08-07 13:56:55 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,14 +1,92 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Double))
+    basic.showLeds(`
+        # # # # .
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # # .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . # # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    시간구간 = input.runningTime() + 5000
+    basic.showString("" + (input.temperature()))
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    시간구간 = input.runningTime() + 5000
+    basic.showNumber(input.lightLevel())
+    basic.showLeds(`
+        # # . # #
+        # # # # #
+        # # # # .
+        . # # # .
+        . # # . .
+        `)
+})
+let 시간구간 = 0
+let initFlag = 0
+시간구간 = 0
+let count001 = input.rotation(Rotation.Pitch)
+input.calibrateCompass()
 basic.forever(function () {
-    basic.pause(1000)
-    if (input.compassHeading() > 45 && input.compassHeading() < 135) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
+    if (input.runningTime() >= 시간구간) {
+        시간구간 = input.runningTime() + 0
+        if (count001 % 4 == 0) {
+            basic.showNumber(input.temperature())
+        }
+        if (count001 % 4 == 1) {
+            basic.showNumber(input.lightLevel())
+        }
+        if (count001 % 4 == 2) {
+            basic.showNumber(input.compassHeading())
+        }
+        if (count001 % 4 == 3) {
+            basic.showNumber(input.rotation(Rotation.Pitch))
+        }
+        count001 += 1
+        시간구간 = input.runningTime() + 10000
     }
-    basic.pause(100)
-    basic.clearScreen()
 })
